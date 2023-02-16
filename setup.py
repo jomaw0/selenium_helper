09d95bb9ac64ec5edbc1c07b51cfec40929e4fa1
Jonas Wolf <jonas.wolf8@me.com>
2023-02-16 17:47:20 +0100

    updated version number

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import setuptools
 
 setuptools.setup(
 	name='selenium_helper',
-	version='0.0.5',
+	version='0.0.6',
 	author='Jonas Wolf',
 	author_email='jonas.wolf8@me.com',
 	description='Helper Class for Selenium',
